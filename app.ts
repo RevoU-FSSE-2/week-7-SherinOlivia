d89e1a5ac20db3a8fef31b96b20876e1f4d05d82
Sherin Olivia <SOChronicle@gmail.com>
2023-08-03 04:56:57 +0700

    feat: added ts (and js) file for assignment's use

---
+++ app.ts
@@ -0,0 +1,7 @@
+console.log("Test");
+
+let transactType = document.getElementById("dataType")
+let transact = document.getElementById("transaction")
+let transactDetails = document.getElementById("transactionDetails")
+let transactAmt = document.getElementById("transactionAmount")
+
