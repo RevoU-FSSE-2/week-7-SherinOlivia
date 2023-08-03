5d44ca369a4cf0fad22ccccd3cb49ccf4eee533d
Sherin Olivia <SOChronicle@gmail.com>
2023-08-04 00:02:35 +0700

    fix: fixed the form submit function

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Test");
+"use strict";
+const financeForm = document.querySelector("#financeForm");
+// :')
+financeForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let transactionType = document.querySelector("#dataType").value;
+    let transaction = document.querySelector("#transaction").value;
+    let transactionDetails = document.querySelector("#transactionDetails").value;
+    let transactionAmount = document.querySelector("#transactionAmount").value;
+    let transactionHistory = (document.querySelector(".transaction-history"));
+    let defaultHistory = transactionHistory === null || transactionHistory === void 0 ? void 0 : transactionHistory.innerHTML;
+    const newHistory = `
+    <li>
+        <h4>${transactionType}</h4>
+        <p>You ${transactionType} Rp. ${transactionAmount} for ${transaction} (Note: ${transactionDetails})</p>
+    </li>
+    `;
+    defaultHistory += newHistory;
+    if (transactionHistory != undefined) {
+        transactionHistory.innerHTML = defaultHistory;
+    }
+});
+// const newH4 = document.createElement("h4")
+// newH4.textContent = "Hallo Semua"
+// console.log(newH4.innerText);
--- app.ts
+++ app.ts
@@ -1,7 +1,39 @@
-console.log("Test");
 
-let transactType = document.getElementById("dataType")
-let transact = document.getElementById("transaction")
-let transactDetails = document.getElementById("transactionDetails")
-let transactAmt = document.getElementById("transactionAmount")
+const financeForm = document.querySelector("#financeForm") as HTMLFormElement
 
+
+// :')
+financeForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let transactionType = (document.querySelector("#dataType") as HTMLSelectElement).value
+    let transaction:string = (document.querySelector("#transaction") as HTMLInputElement).value
+    let transactionDetails:string = (document.querySelector("#transactionDetails") as HTMLInputElement).value
+    let transactionAmount:string = (document.querySelector("#transactionAmount") as HTMLInputElement).value
+
+    let transactionHistory = (document.querySelector(".transaction-history")) as HTMLUListElement
+
+
+    let defaultHistory = transactionHistory?.innerHTML
+    const newHistory = `
+    <li>
+        <h4>${transactionType}</h4>
+        <p>You ${transactionType} Rp. ${transactionAmount} for ${transaction} (Note: ${transactionDetails})</p>
+    </li>
+    `
+    defaultHistory += newHistory
+    if (transactionHistory != undefined) {
+        transactionHistory.innerHTML = defaultHistory;
+    }
+})
+
+
+
+
+
+
+
+// const newH4 = document.createElement("h4")
+// newH4.textContent = "Hallo Semua"
+
+// console.log(newH4.innerText);
